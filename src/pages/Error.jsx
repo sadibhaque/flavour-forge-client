cc592c3a2bbf198cde00c93ef6baaba889a41668
sadibhaque <sadibhaquee@gmail.com>
2025-05-24 09:43:36 +0600

    removed navbr from error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 import Lottie from "lottie-react";
 import error from "../../public/error.json";
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 
 const Error = () => {
     return (
         <div>
-            <Navbar></Navbar>
             <div className="flex flex-col items-center justify-center h-screen">
                 <Lottie animationData={error} />
             </div>
